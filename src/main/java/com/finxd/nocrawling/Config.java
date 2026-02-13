01705c6fe325bff4e2d6d107d1358343568d18f1
Finxd <timphaapsaari@gmail.com>
2026-02-13 15:30:10 +0200

    old eye level config + bugfixes

---
+++ src/main/java/com/finxd/nocrawling/Config.java
@@ -0,0 +1,41 @@
+package com.finxd.nocrawling;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
+
+import static com.finxd.nocrawling.NoCrawling.MODID;
+
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config {
+
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    public static boolean oldEyeHeight;
+
+    public static final ForgeConfigSpec.BooleanValue OLD_EYE_HEIGHT = BUILDER
+            .comment(" Should player's crouching eye level be at it's old value?")
+            .comment(" (this option clashes with Nostalgic Tweaks' \"oldSneaking\" tweak, the animation breaks a little, so both options shouldn't be used at the same time)")
+            .comment(" true = 1,54 (old value), false = 1.27 (modern vanilla value)")
+            .define("oldEyeHeight", false);
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    @SubscribeEvent
+    public static void onConstructModEvent(final FMLConstructModEvent event) {
+        final ModLoadingContext context = ModLoadingContext.get();
+        context.registerConfig(ModConfig.Type.SERVER, SPEC);
+    }
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+        oldEyeHeight = OLD_EYE_HEIGHT.get();
+    }
+}
+
+
--- src/main/java/com/finxd/nocrawling/mixin/PlayerMixin.java
+++ src/main/java/com/finxd/nocrawling/mixin/PlayerMixin.java
@@ -1,15 +1,24 @@
 package com.finxd.nocrawling.mixin;
 
+import com.finxd.nocrawling.Config;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Pose;
+import net.minecraft.world.entity.player.Abilities;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
 import java.util.Map;
 
 import static net.minecraft.world.entity.Pose.*;
@@ -17,12 +26,18 @@ import static net.minecraft.world.entity.player.Player.STANDING_DIMENSIONS;
 
 @Mixin(Player.class)
 
-public abstract class PlayerMixin {
+public abstract class PlayerMixin extends LivingEntity {
+    private final Abilities abilities = new Abilities();
+    @Nullable
+    private Pose forcedPose;
 
-    //changed crouch height from 1.5 to 1.65 blocks like in older versions
+    protected PlayerMixin(EntityType<? extends LivingEntity> p_20966_, Level p_20967_) {
+        super(p_20966_, p_20967_);
+    }
+
+    //crouch height is now the same as standing height instead of 1.65 due to a bug with ladders that allows the player to phase through the block above
     //also changed swimming pose to the same as standing so the player can't "crawl" in 1 block tall spaces
 
-    //this has a minor side effect: whenever a player is crouching and a trapdoor is shut on the player's head, the player's player model remains standing but the eye height goes slightly lower
     //also, whenever swimming, the player no longer fits in 1 block tall spaces
     @Unique
     private static final Map<Pose, EntityDimensions> POSES = ImmutableMap.<Pose, EntityDimensions>builder()
@@ -31,7 +46,8 @@ public abstract class PlayerMixin {
             .put(FALL_FLYING, EntityDimensions.scalable(0.6F, 0.6F))
             .put(SWIMMING, STANDING_DIMENSIONS)
             .put(SPIN_ATTACK, EntityDimensions.scalable(0.6F, 0.6F))
-            .put(Pose.CROUCHING, EntityDimensions.scalable(0.6F, 1.65F))
+//            .put(Pose.CROUCHING, EntityDimensions.scalable(0.6F, 1.65F))
+            .put(Pose.CROUCHING, STANDING_DIMENSIONS)
             .put(Pose.DYING, EntityDimensions.fixed(0.2F, 0.2F)).build();
 
     @Inject(method = "getDimensions", at = @At("HEAD"), cancellable = true)
@@ -39,9 +55,39 @@ public abstract class PlayerMixin {
         cir.setReturnValue(POSES.getOrDefault(pPose, STANDING_DIMENSIONS));
     }
 
-//    //player no longer swims
-//    @Inject(method = "isSwimming", at = @At("HEAD"), cancellable = true)
-//    public void isSwimming(CallbackInfoReturnable<Boolean> cir) {
-//        cir.cancel();
-//    }
+    //old crouching eye height
+    //clashes with Nostalgic Tweaks' "oldSneaking" tweak
+    @Inject(method = "getStandingEyeHeight", at = @At("HEAD"), cancellable = true)
+    public void getStandingEyeHeight(Pose pose, EntityDimensions p_36260_, CallbackInfoReturnable<Float> cir) {
+        if (pose == CROUCHING && Config.OLD_EYE_HEIGHT.get()) {
+            cir.setReturnValue(1.62F - 0.08F);
+        }
+    }
+
+    //fixed player getting stuck in either standing or crouching position after a trapdoor is shut on the player's head
+    //still doesn't fix the issue where the player inside the trapdoor stays standing
+    //only the player inside the trapdoor sees this, it looks normal to other players (why does this happen?)
+    @Inject(method = "updatePlayerPose", at = @At("HEAD"))
+    protected void updatePlayerPose(CallbackInfo ci) {
+        if(forcedPose != null) {
+            this.setPose(forcedPose);
+            return;
+        }
+        Pose pose;
+        if (this.isFallFlying()) {
+            pose = Pose.FALL_FLYING;
+        } else if (this.isSleeping()) {
+            pose = Pose.SLEEPING;
+        } else if (this.isSwimming()) {
+            pose = Pose.SWIMMING;
+        } else if (this.isAutoSpinAttack()) {
+            pose = Pose.SPIN_ATTACK;
+        } else if (this.isShiftKeyDown() && !this.abilities.flying) {
+            pose = Pose.CROUCHING;
+        } else {
+            pose = Pose.STANDING;
+        }
+
+        this.setPose(pose);
+    }
 }
